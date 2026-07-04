aa4d3944edf8de53bf0e3cc2f3d5ceef0fedd07c
Dmitriy <dfajl@mail.ru>
2026-07-04 14:33:34 +0300

    правит тип

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,13 +1,13 @@
 import { restaurants } from "../../mock/restaurantsList.js";
 import { RestaurantItem } from "./RestaurantItem/RestaurantItem.tsx";
-import type { Restaurant } from "./types.ts";
+import type { TRestaurant } from "./types.ts";
 
 export const RestaurantList = () => {
 	return (
 		<div>
 			<h1>Restaurants List</h1>
 			<ul>
-				{restaurants.map((restaurant: Restaurant) => (
+				{restaurants.map((restaurant: TRestaurant) => (
 					<RestaurantItem
 						key={restaurant.id}
 						restaurant={restaurant}
