ce9fdf09352ac1164a0fd6fb220a988cd9a51d81
Dmitriy <dfajl@mail.ru>
2026-07-07 13:25:48 +0300

    вносит корректировки: табы уходят в UI, поиск ресторана по ID

--- src/components/RestaurantList/RestaurantTabs/types.ts
+++
@@ -1,8 +0,0 @@
-import type { TRestaurant } from "../restaurantTypes";
-
-export type TRestaurantTabsProps = {
-	restaurants: TRestaurant[];
-	style: React.CSSProperties;
-	onChange: (restaurant: TRestaurant) => void;
-	activeRestaurantId: string | undefined;
-};
--- src/components/RestaurantList/RestaurantsPage.tsx
+++ src/components/RestaurantList/RestaurantsPage.tsx
@@ -1,26 +1,43 @@
 import { useState } from "react";
 import { restaurants } from "../../mock/restaurantsList.ts";
+import { Tabs } from "../UI/Tabs/Tabs.tsx";
 import { RestaurantItem } from "./RestaurantItem/RestaurantItem.tsx";
-import { RestaurantTabs } from "./RestaurantTabs/RestaurantTabs.tsx";
 import type { TRestaurant } from "./restaurantTypes.ts";
 
 export const RestaurantsPage = () => {
-	const firstRestaurant = restaurants[0] || null;
-	const [currentRestaurant, setCurrentRestaurant] = useState<TRestaurant | null>(firstRestaurant);
+	const [activeRestaurantId, setActiveRestaurantId] = useState<string | null>(
+		() => restaurants[0]?.id ?? null,
+	);
+
+	const currentRestaurant: TRestaurant | null =
+		restaurants.find(({ id }) => id === activeRestaurantId) ?? null;
+
+	const restaurantTabs = restaurants.map(({ id, name }) => ({
+		id,
+		label: name,
+	}));
+
+	const handleSetActiveRestaurantId = (restaurantId: string) => {
+		if (activeRestaurantId === restaurantId) {
+			return;
+		}
+
+		setActiveRestaurantId(restaurantId);
+	};
 
 	return (
 		<div>
 			<h1>Current Restaurant</h1>
-			<RestaurantTabs
-			    restaurants={restaurants}
-				activeRestaurantId={currentRestaurant?.id}
+			<Tabs
+				items={restaurantTabs}
+				activeId={activeRestaurantId}
 				style={{
 					display: "flex",
 					justifyContent: "center",
 					alignItems: "center",
-					gap: "10px"
+					gap: "10px",
 				}}
-				onChange={setCurrentRestaurant}
+				onChange={handleSetActiveRestaurantId}
 			/>
 			{currentRestaurant && <RestaurantItem restaurant={currentRestaurant}/>}
 		</div>
